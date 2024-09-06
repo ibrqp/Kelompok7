bb7d973c1cebbe6e8b4f57488390476ffd55368d
ibrqp <muchammadibrahimalamin@gmail.com>
2024-09-06 17:52:46 +0700

    commit pertama saya lho

---
+++ Desktop/praktik daspro/minggu 1/Latihan16.java
@@ -0,0 +1,19 @@
+/**
+ * Latihan16
+ */
+public class Latihan16 {
+
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");    
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");    
+        System.out.println("---------------------------------------------------------");    
+        System.out.println("Nama    : Muchammad Ibrahim Al Amin");    
+        System.out.println("NIM     : 244107020062");    
+        System.out.println("Kelas   : 1G");    
+        System.out.println("No Absen: 16");    
+        System.out.println("Prodi   : Teknik Informatika");    
+        System.out.println("Alamat  : Beji, Kab. Pasuruan");    
+        System.out.println("No HP   : 085784818488");    
+        System.out.println("Hobi    : Olahraga");    
+    }
+}
---
+++ Desktop/praktik daspro/minggu 1/MyFirstJava16.java
@@ -0,0 +1,5 @@
+public class MyFirstJava16{
+    public static void main(String[] args){
+        System.out.println("Nama Saya Muchammad Ibrahim Al Amin");
+    }
+}
